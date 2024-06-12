acef6c60ca7fd2654af6023ba88918ca5f26c267
mattrende <mrende@gmail.com>
2024-06-11 17:08:09 -0700

    added my first class

--- menutime/core/views.py
+++ menutime/core/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,Blueprint
 from menutime import db
+from menutime.my_classes import QueryBarChart
 from google.cloud.firestore_v1 import aggregation
 from google.cloud.firestore_v1.base_query import FieldFilter
+import base64
 
 
 core = Blueprint('core',__name__)
@@ -21,23 +23,29 @@ def customdetails():
 @core.route('/about')
 def about():
     ## Query number of meals in DB
-    collection_ref = db.collection("meals")
-    query = collection_ref.where(filter=FieldFilter("name", "!=", ""))
-    aggregate_query = aggregation.AggregationQuery(query)
-    aggregate_query.count(alias="all")
+    # collection_ref = db.collection("meals")
+    # query = collection_ref.where(filter=FieldFilter("name", "!=", ""))
+    # aggregate_query = aggregation.AggregationQuery(query)
+    # aggregate_query.count(alias="all")
 
-    results = aggregate_query.get()
-    for result in results:
-        count_of_meals = result[0].value
+    # results = aggregate_query.get()
+    # for result in results:
+    #     count_of_meals = result[0].value
 
-    ## Query number of menus in DB
-    collection_ref = db.collection("selections")
-    query = collection_ref.where(filter=FieldFilter("created_date", "!=", ""))
-    aggregate_query = aggregation.AggregationQuery(query)
-    aggregate_query.count(alias="all")
+    # ## Query number of menus in DB
+    # collection_ref = db.collection("selections")
+    # query = collection_ref.where(filter=FieldFilter("created_date", "!=", ""))
+    # aggregate_query = aggregation.AggregationQuery(query)
+    # aggregate_query.count(alias="all")
 
-    menu_results = aggregate_query.get()
-    for result in menu_results:
-        count_of_menus = result[0].value
+    # menu_results = aggregate_query.get()
+    # for result in menu_results:
+    #     count_of_menus = result[0].value
 
-    return render_template('about.html', num_meals = count_of_meals, num_menus = count_of_menus)
+    query = QueryBarChart()
+    count_of_menus = query.query_num_menus()
+    count_of_meals = query.query_num_meals()
+    create_chart = query.run_query_and_plot()
+    chart_image = base64.b64encode(create_chart.getvalue()).decode('utf-8')
+
+    return render_template('about.html', num_menus=count_of_menus, num_meals=count_of_meals, image=chart_image)
---
+++ menutime/my_classes.py
@@ -0,0 +1,83 @@
+from menutime import db
+import matplotlib.pyplot as plt
+from google.cloud.firestore_v1 import aggregation
+from google.cloud.firestore_v1.base_query import FieldFilter
+from io import BytesIO
+
+
+class QueryBarChart:
+    def __init__(self):
+        self.db = db
+
+    def query_num_menus(self):
+        ## Query number of menus in DB
+        collection_ref = db.collection("selections")
+        query = collection_ref.where(filter=FieldFilter("created_date", "!=", ""))
+        aggregate_query = aggregation.AggregationQuery(query)
+        aggregate_query.count(alias="all")
+
+        menu_results = aggregate_query.get()
+        for result in menu_results:
+            count_of_menus = result[0].value
+        return count_of_menus
+
+    def query_num_meals(self):
+        ## Query number of meals in DB
+        collection_ref = db.collection("meals")
+        query = collection_ref.where(filter=FieldFilter("category", "!=", ""))
+        aggregate_query = aggregation.AggregationQuery(query)
+        aggregate_query.count(alias="all")
+
+        meal_results = aggregate_query.get()
+        for result in meal_results:
+            count_of_meals = result[0].value
+        return count_of_meals
+    
+    def run_query_and_plot(self):
+        
+
+        meal_names_query = self.db.collection("meals")
+        meals = [doc.to_dict() for doc in meal_names_query.stream()]
+
+        # Assuming each meal dictionary has a 'category' key
+        category_counts = {}
+        for meal in meals:
+            category = meal['category']
+            if category in category_counts:
+                category_counts[category] += 1
+            else:
+                category_counts[category] = 1
+
+        labels = list(category_counts.keys())
+        counts = list(category_counts.values())
+
+        # Use a non-interactive backend to avoid GUI issues in threads
+        plt.switch_backend('Agg')
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        bars = ax.bar(labels, counts, color=plt.get_cmap('Pastel2').colors)
+        ax.set_xlabel('Categories')
+        ax.set_title('Number of Meals Currently Available by Category')
+
+        # Apply spine and tick formatting
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_color('#DDDDDD')
+        ax.tick_params(bottom=False, left=False)
+
+        # Add horizontal grid lines
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(True, color='#EEEEEE')
+        ax.xaxis.grid(False)
+
+        for bar in bars:
+            yval = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width()/2, yval, int(yval), va='bottom')  # va: vertical alignment
+
+        # Instead of saving the plot, return it as a response object that can be used in Flask
+        img = BytesIO()
+        plt.savefig(img, format='png', bbox_inches='tight')
+        plt.close(fig)
+        img.seek(0)
+        return img
